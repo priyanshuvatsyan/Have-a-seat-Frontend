27a8e4118c19c95e2d08fb25f2a85551c5f60da4
Priyanshu <56836660+priyanshuvatsyan@users.noreply.github.com>
2024-12-17 17:38:18 +0530

    fixed login path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './screens/Home';
-import Login from './screens/login';
+import Login from './screens/Login'
 import SignUp from './screens/SignUp';
 import { CardProvider } from './componants/ContextReducer';
 import MyOrders from './screens/MyOrders';
--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
+
 import Badge from 'react-bootstrap/Badge';
 import { useState } from "react";
 import Model from "../Model";
